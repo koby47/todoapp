c1e641ebbee4dc27d5835e3f30a645f2fe0e0267
Sam Quaye <samuelmensahquaye@gmail.com>
2025-05-03 17:42:08 +0000

    delete functionality

--- todoapp/src/components/Todoitem.jsx
+++ todoapp/src/components/Todoitem.jsx
@@ -1,10 +1,16 @@
 import styles from "./todoitem.module.css";
 
-export default function TodoItem({item}){
+export default function TodoItem({item,todos,setTodos}){
+    function handleDelete(item){
+        console.log("Delete button clicked",item);
+        setTodos( todos.filter((todo) => todo !== item));
+       
+    }
     return ( <div className={styles.item}>
     <div className={styles.itemName}>{item}
     <span >
-        <button className={styles.deleteButton}> x</button>
+        <button  onClick={() => handleDelete(item)}
+         className={styles.deleteButton}> x</button>
     </span>
     </div>
     
